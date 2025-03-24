fdbf50c5b3c2a3d53318c11cf8354fdb14868346
Seva Koshmiakov <vsevolod.koshmiakov@dif.tech>
2025-03-24 03:00:15 +0100

    No more magic numbers

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -6,9 +6,11 @@ import ru.yandex.architectureproject.data.repository.TaskRepository
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
 ) {
+    private val delayTime = 10_000L
+
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        delay(10_000)
+        delay(delayTime)
         repository.deleteTask(taskId)
     }
 }
