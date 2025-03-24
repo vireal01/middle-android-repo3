ca5ad5a670ef1b5c37ea5e2dfee126c4ef3e39d4
Seva Koshmiakov <vsevolod.koshmiakov@dif.tech>
2025-03-24 02:58:13 +0100

    Completed task

--- app/src/main/java/ru/yandex/architectureproject/presentation/viewmodel/TaskViewModel.kt
+++ app/src/main/java/ru/yandex/architectureproject/presentation/viewmodel/TaskViewModel.kt
@@ -3,12 +3,15 @@ package ru.yandex.architectureproject.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.yandex.architectureproject.domain.AddTaskUseCase
@@ -20,31 +23,52 @@ import ru.yandex.architectureproject.presentation.state.TaskAction
 import ru.yandex.architectureproject.presentation.state.TaskState
 
 class TaskViewModel(
-    private val addTaskUseCase: AddTaskUseCase,
-    private val deleteTaskUseCase: DeleteTaskUseCase,
-    private val getAllTasksUseCase: GetAllTasksUseCase,
-    private val completeTaskUseCase: CompleteTaskUseCase,
-    private val incompleteTaskUseCase: IncompleteTaskUseCase,
-    private val ioDispatcher: CoroutineDispatcher,
+  private val addTaskUseCase: AddTaskUseCase,
+  private val deleteTaskUseCase: DeleteTaskUseCase,
+  private val getAllTasksUseCase: GetAllTasksUseCase,
+  private val completeTaskUseCase: CompleteTaskUseCase,
+  private val incompleteTaskUseCase: IncompleteTaskUseCase,
+  private val ioDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
-    private val _state = MutableStateFlow<TaskState>(TaskState.Loading)
-    val state: StateFlow<TaskState> = _state.asStateFlow()
+  private val _state = MutableStateFlow<TaskState>(TaskState.Loading)
+  val state: StateFlow<TaskState> = _state.asStateFlow()
 
-    init {
-        reduce(TaskAction.LoadTasks)
-    }
+  private val tasksToBeDeleted = mutableMapOf<Int, Job>()
 
-    fun reduce(action: TaskAction) {
-        // TODO: Здесь должна быть обработка действий
-    }
+  init {
+    reduce(TaskAction.LoadTasks)
+  }
 
-    private suspend fun loadTasks() {
-        withContext(ioDispatcher) {
-            getAllTasksUseCase()
-                .distinctUntilChanged()
-                .onStart { _state.value = TaskState.Loading }
-                .catch { e -> _state.value = TaskState.Error(e.message ?: "Ошибка загрузки") }
-                .collect { tasks -> _state.value = TaskState.Loaded(tasks) }
+  fun reduce(action: TaskAction) {
+    viewModelScope.launch {
+      withContext(ioDispatcher) {
+        // мне кажется, что IO dispatcher подходит под любой из типов TaskAction, поэтому я добавил его для всех вариантов TaskAction.
+        when (action) {
+          is TaskAction.LoadTasks -> loadTasks()
+          is TaskAction.AddTask -> addTaskUseCase(action.task)
+
+          is TaskAction.UpdateTaskStatus -> {
+            if (action.isDone) {
+              tasksToBeDeleted[action.taskId] = this.coroutineContext.job
+              completeTaskUseCase(action.taskId)
+            } else {
+              tasksToBeDeleted[action.taskId]?.cancel()
+              incompleteTaskUseCase(action.taskId)
+            }
+          }
+
+          is TaskAction.DeleteTask -> deleteTaskUseCase(action.taskId)
         }
+      }
+
     }
+  }
+
+  private suspend fun loadTasks() {
+    getAllTasksUseCase()
+      .distinctUntilChanged()
+      .onStart { _state.value = TaskState.Loading }
+      .catch { e -> _state.value = TaskState.Error(e.message ?: "Ошибка загрузки") }
+      .collect { tasks -> _state.value = TaskState.Loaded(tasks) }
+  }
 }
